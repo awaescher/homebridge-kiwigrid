6e47d7aa919e8bd750c5bdc8d8671e336456970e
Andreas Wäscher <awaescher@sodacore.net>
2020-06-11 22:28:44 +0200

    Update accessories on plugin start

--- src/KiwigridHomebridgePlatform.ts
+++ src/KiwigridHomebridgePlatform.ts
@@ -108,8 +108,9 @@ export class KiwigridHomebridgePlatform implements DynamicPlatformPlugin {
       this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
       // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-      // existingAccessory.context.device = device;
-      // this.api.updatePlatformAccessories([existingAccessory]);
+      existingAccessory.context.device = battery;
+      this.api.updatePlatformAccessories([existingAccessory]);
+
       // create the accessory handler for the restored accessory
       // this is imported from `platformAccessory.ts`
       new KiwiBatteryServiceAccessory(this.log, this, existingAccessory);
